9478f7294dfb333600c8aa31f68cb0e31e9f8c33
jungh150 <jungh150@naver.com>
2024-10-15 18:49:52 +0900

    feat: Inquiry 엔티티 생성

---
+++ src/main/java/umc/study/domain/Inquiry.java
@@ -0,0 +1,24 @@
+package umc.study.domain;
+
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Inquiry extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
