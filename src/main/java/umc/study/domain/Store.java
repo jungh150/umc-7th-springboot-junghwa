6278f1689723804fbc572dc7cf36e5270805da81
jungh150 <jungh150@naver.com>
2024-10-15 18:17:29 +0900

    feat: Store 엔티티 생성

---
+++ src/main/java/umc/study/domain/Store.java
@@ -0,0 +1,23 @@
+package umc.study.domain;
+
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    private Float rate;
+}
