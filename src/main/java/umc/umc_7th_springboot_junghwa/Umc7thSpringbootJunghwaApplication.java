65faa51800f10d9757f2c4e469407647fcb4610f
jungh150 <jungh150@naver.com>
2024-10-15 17:00:35 +0900

    initial setting

---
+++ src/main/java/umc/umc_7th_springboot_junghwa/Umc7thSpringbootJunghwaApplication.java
@@ -0,0 +1,13 @@
+package umc.umc_7th_springboot_junghwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc7thSpringbootJunghwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc7thSpringbootJunghwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/umc_7th_springboot_junghwa/Umc7thSpringbootJunghwaApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.umc_7th_springboot_junghwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc7thSpringbootJunghwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
