ba885e535ea7e6eefec5c5ec877948f2cb97329b
jungh150 <jungh150@naver.com>
2024-11-05 19:21:06 +0900

    feat: QueryDSL 설정 파일 생성

---
+++ src/main/java/umc/study/config/QueryDSLConfig.java
@@ -0,0 +1,12 @@
+package umc.study.config;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDSLConfig {
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory(entityManager);
+    }
+}
