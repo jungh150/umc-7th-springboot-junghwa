65768ab3536341de3016b11c800e8977a2df83da
jungh150 <jungh150@naver.com>
2024-11-19 00:29:15 +0900

    feat: PreferConverter 생성 및 UserCommandServiceImpl 수정

--- src/main/java/umc/study/service/UserService/UserCommandServiceImpl.java
+++ src/main/java/umc/study/service/UserService/UserCommandServiceImpl.java
@@ -2,18 +2,41 @@ package umc.study.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import umc.study.apiPayload.code.status.ErrorStatus;
+import umc.study.apiPayload.exception.handler.FoodHandler;
+import umc.study.converter.UserConverter;
+import umc.study.converter.PreferConverter;
+import umc.study.domain.Food;
 import umc.study.domain.User;
+import umc.study.domain.mapping.Prefer;
+import umc.study.repository.FoodRepository;
 import umc.study.repository.UserRepository;
 import umc.study.web.dto.UserRequestDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserCommandServiceImpl implements UserCommandService {
 
     private final UserRepository userRepository;
+    private final FoodRepository foodRepository;
 
     @Override
+    @Transactional
     public User signupUser(UserRequestDTO.SignupDto request) {
-        return null;
+        User newUser = UserConverter.toUser(request);
+        List<Food> foodList = request.getPreferList().stream()
+                .map(category -> {
+                    return foodRepository.findById(category).orElseThrow(() -> new FoodHandler(ErrorStatus.FOOD_CATEGORY_NOT_FOUND));
+                }).collect(Collectors.toList());
+
+        List<Prefer> preferList = PreferConverter.toPreferList(foodList);
+
+        preferList.forEach(prefer -> {prefer.setUser(newUser);});
+
+        return userRepository.save(newUser);
     }
 }
