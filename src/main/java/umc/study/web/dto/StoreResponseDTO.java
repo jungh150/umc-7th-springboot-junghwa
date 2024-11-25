ced0b003b6d373d21cd117d1bf33badd984fae6e
Junghwa <jungh150@naver.com>
2024-11-26 02:40:24 +0900

    feat: ReviewPreViewListDTO, ReviewPreViewDTO 추가

--- src/main/java/umc/study/web/dto/StoreResponseDTO.java
+++ src/main/java/umc/study/web/dto/StoreResponseDTO.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class StoreResponseDTO {
 
@@ -15,6 +16,30 @@ public class StoreResponseDTO {
     @AllArgsConstructor
     public static class CreateStoreResultDTO{
         Long storeId;
+        LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO {
+        List<ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO {
+        String ownerNickname;
+        Float score;
+        String body;
         LocalDateTime createdAt;
     }
 }
