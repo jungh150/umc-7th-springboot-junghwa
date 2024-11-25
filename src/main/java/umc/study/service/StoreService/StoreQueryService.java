4936b2529bc86db498e25b6fc93b0cd363cd1d78
Junghwa <jungh150@naver.com>
2024-11-26 03:18:00 +0900

    feat: StoreQueryService, StoreQueryServiceImpl에 getReviewList 추가

--- src/main/java/umc/study/service/StoreService/StoreQueryService.java
+++ src/main/java/umc/study/service/StoreService/StoreQueryService.java
@@ -1,10 +1,13 @@
 package umc.study.service.StoreService;
 
+import org.springframework.data.domain.Page;
 import umc.study.domain.Store;
+import umc.study.domain.mapping.Review;
 
 import java.util.Optional;
 
 public interface StoreQueryService {
 
     Optional<Store> findStore(Long id);
+    Page<Review> getReviewList(Long StoreId, Integer page);
 }
--- src/main/java/umc/study/service/StoreService/StoreQueryServiceImpl.java
+++ src/main/java/umc/study/service/StoreService/StoreQueryServiceImpl.java
@@ -1,9 +1,14 @@
 package umc.study.service.StoreService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import umc.study.domain.Store;
+import umc.study.domain.mapping.Review;
+import umc.study.repository.ReviewRepository;
+import umc.study.repository.StoreRepository;
 
 import java.util.Optional;
 
@@ -12,8 +17,20 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class StoreQueryServiceImpl implements StoreQueryService {
 
+    private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
+
     @Override
     public Optional<Store> findStore(Long id) {
         return Optional.empty();
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long StoreId, Integer page) {
+
+        Store store = storeRepository.findById(StoreId).get();
+
+        Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return StorePage;
     }
 }
