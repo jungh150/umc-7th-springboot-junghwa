05198f7763b7f19a2009a0969d117491edb9a066
jungh150 <jungh150@naver.com>
2024-12-17 17:16:51 +0900

    feat: UserViewController 생성

---
+++ src/main/java/umc/study/web/controller/UserViewController.java
@@ -0,0 +1,36 @@
+package umc.study.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import umc.study.service.UserService.UserCommandService;
+import umc.study.web.dto.UserRequestDTO;
+
+@Controller
+public class UserViewController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage(Model model) {
+        model.addAttribute("userSignupDto", new UserRequestDTO.SignupDto());
+        return "signup";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
